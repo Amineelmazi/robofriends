3a89ee87b3411b5b5d257747c78b10a65e7aa65d
amineelmazi <amineelmazi@gmail.com>
2020-04-23 20:02:54 +0000

    adding starting project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots =[
+	{
+		id: 1,
+		name: 'Amine Elmazi',
+		username: 'Am.El',
+		email: 'Amine@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Elmehdi Rouai',
+		username: 'Elmehdi.Elmontadar',
+		email: 'Medi@vert.com'
+	},
+	{
+		id: 3,
+		name: 'Uness Tailaba',
+		username: 'Y.El',
+		email: 'Chaye5@Uness.com'
+	},
+	{
+		id: 75,
+		name: 'Abdelhazi Lmicanic',
+		username: 'Toli.Abdo',
+		email: 'Abdelhazi@gmail.com'
+	},
+	{
+		id: 545,
+		name: 'Hamid S7i7',
+		username: 'Hmd.Entraineur',
+		email: 'Hamid@0647891247.com'
+	},
+	{
+		id: 5425,
+		name: 'Messi ',
+		username: 'leo.Msous',
+		email: 'Messi@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Mounir Aboufiker',
+		username: 'Swaye3',
+		email: 'Moul.Lmektaba@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Soulaimane Elmazi',
+		username: 'Sou.Sou',
+		email: 'Soulaimane@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'Simo Elmazi',
+		username: 'Moha.El',
+		email: 'Mohamed@gmail.com'
+	},
+	{
+		id: 100,
+		name: 'Adam Elmazi',
+		username: 'ADam.Dasser',
+		email: 'Dsara@gmail.com'
+	},
+]
